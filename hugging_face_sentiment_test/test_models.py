de671f05e7ade73aadd0911a1ef348b594d51bf1
Joshua Sperry <jrsperry@halosight.com>
2023-01-22 10:38:54 -0700

    initial

--- hugging_face_sentiment_test/test_models.py
+++ hugging_face_sentiment_test/test_models.py
@@ -13,6 +13,8 @@ logger.addHandler(handler)
 log_level = os.environ.get("LOGGING_LEVEL", "INFO")
 logger.setLevel(log_level)
 
+# if you don't have the model downloaded, set MODEL_PATH to 'cardiffnlp/twitter-roberta-base-sentiment'
+# and set TRANSFORMERS_OFFLINE to 0
 os.environ['TRANSFORMERS_OFFLINE'] = '1'
 MODEL_PATH = "./hugging_face_sentiment_test/resources/model"
 
