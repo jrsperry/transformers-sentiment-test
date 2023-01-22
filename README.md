bc4d8a25e84b5a9340f839914407a225feaebfe9
Joshua Sperry <jrsperry@halosight.com>
2023-01-22 10:37:28 -0700

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Huggingface Sentiment Test
+
+This is designed to be a test project to test the performance of out-of-the-box transformers. The model is stored here 
+in the project to decrease startup time when running in the cloud but i can't store those large of files in my github...
+
+Env vars:
+- ITERATIONS = how many iterations to run (default 25)
+- NUMBER_SENTENCES = how many sentences per iteration (default 100)
---
+++ hugging_face_sentiment_test/test_models.py
@@ -0,0 +1,36 @@
+import logging
+from transformers import pipeline
+
+import os
+import pandas as pd
+import time
+import sys
+
+logger = logging.getLogger("sentiment_tester")
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(levelname)-8s :   %(message)s'))
+logger.addHandler(handler)
+log_level = os.environ.get("LOGGING_LEVEL", "INFO")
+logger.setLevel(log_level)
+
+os.environ['TRANSFORMERS_OFFLINE'] = '1'
+MODEL_PATH = "./hugging_face_sentiment_test/resources/model"
+
+logger.info("loading model")
+classifier = pipeline(task="sentiment-analysis", model=MODEL_PATH, tokenizer=MODEL_PATH, max_length=512,
+                      truncation=True)
+
+number_sentences = os.environ.get('NUMBER_SENTENCES', '100')
+number_sentences_int = int(number_sentences)
+
+text_sentences = list(pd.read_csv('./hugging_face_sentiment_test/resources/amazon_sentences.csv')['text'])[:number_sentences_int]
+
+iterations = os.environ.get('ITERATIONS', '25')
+iterations_int = int(iterations)
+logger.info(f"running {iterations_int} iterations on {len(text_sentences)} sentences")
+
+for i in range(iterations_int):
+    beforeAll = time.time()
+    preds = classifier(text_sentences)
+    afterall = time.time()
+    logger.info(f"time to do {len(text_sentences)} sentences:  {afterall - beforeAll}s")
